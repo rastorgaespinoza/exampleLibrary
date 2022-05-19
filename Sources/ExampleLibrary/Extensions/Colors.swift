f753874ed45fbb7c40ab741531396d2f648a11ef
Rodrigo <r.astorgaespinoz@gmail.com>
2022-05-19 12:39:52 -0400

    add colors to library

---
+++ Sources/ExampleLibrary/Extensions/Colors.swift
@@ -0,0 +1,74 @@
+//
+//  File.swift
+//  
+//
+//  Created by Rodrigo Astorga on 19-05-22.
+//
+
+import SwiftUI
+
+extension Color {
+  // MARK: Complementary group
+  /// Complementary Pastel Magenta hex color: #F9E4ED
+  static let pastelMagenta = Color("pastel_magenta")
+  /// Complementary Pastel Mint hex color: #E6F4F1
+  static let pastelMint = Color("pastel_mint")
+  /// Complementary Pastel Terra hex color: #FEF6F0
+  static let pastelTerra = Color("pastel_terra")
+  /// Complementary Pastel cyan hex color: #E6F5FC
+  static let pastelCyan = Color("pastel_cyan")
+
+  // MARK: Feedback group
+  /// Feedback green paris hex color: #009372
+  static let greenParis = Color("green_paris")
+  /// Feedback red paris hex color: #EE595A
+  static let redParis = Color("red_paris")
+  /// Feedback red paris hex color: #E96716
+  static let orangeParis = Color("orange_paris")
+
+  // MARK: Grayscale group
+  /// Grayscale Background paris hex color: #F9F9F9
+  static let backgroundParis = Color("background_paris")
+  /// Grayscale Black paris hex color: #1A1A1A
+  static let blackParis = Color("black_paris")
+  /// Grayscale Light gray border hex color: #D2D2D2
+  static let lightGrayBorder = Color("light_gray_border")
+  /// Grayscale Silver hex color: #4D4D4D
+  static let silver = Color("silver")
+  /// Grayscale Skeleton hex color: #E2E2E2
+  static let skeleton = Color("skeleton")
+  /// Grayscale Pearl gray hex color: #F2F2F2
+  static let pearlGray = Color("pearl_gray")
+  /// Grayscale Dark gray hex color: #818180
+  static let darkGray = Color("dark_gray")
+  /// Grayscale High white hex color: Any: #FFFFFF - Dark: #1A1A1A
+  static let whiteParis = Color("high_white")
+  /// Grayscale High white hex color: Any: #FFFFFF - Dark: #000000
+  static let whiteTwoParis = Color("Background1")
+
+  // MARK: Primary group
+  /// Primary Cyan hex color: #006DFF
+  static let cyan = Color("cyan_paris")
+  /// Primary Cyan strong hex color: #0057CB
+  static let cyanStrong = Color("cyan_strong")
+
+  // MARK: Secondary group
+  /// Secondary Indigo hex color: #00355B
+  static let indigo = Color("indigo")
+  /// Secondary Ratings fill hex color: #FFC833
+  static let ratingsFill = Color("ratings_fill")
+
+  // MARK: Gammification group
+  /// Gammification level 1 hex color: #00856A
+  static let gammificationLevel1 = Color("gammification_level1")
+  /// Gammification level 2  hex color: #228091
+  static let gammificationLevel2 = Color("gammification_level2")
+  /// Gammification level 3 hex color: #5E5EF7
+  static let gammificationLevel3 = Color("gammification_level3")
+  /// Gammification level 4  hex color: #541CCC
+  static let gammificationLevel4 = Color("gammification_level4")
+  /// Gammification level 5 hex color: #3E4F5B
+  static let gammificationLevel5 = Color("gammification_level5")
+  /// Gammification level 6  hex color: #00355B
+  static let gammificationLevel6 = Color("gammification_level6")
+}
