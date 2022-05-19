6b14b46daef400137926e4fa8f62077bb648b717
Rodrigo <r.astorgaespinoz@gmail.com>
2022-05-19 13:58:54 -0400

    update bundle colors

--- Package.swift
+++ Package.swift
@@ -24,10 +24,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
           name: "ExampleLibrary",
-          dependencies: [],
-          resources: [
-            .process("Resources/ParisAssets.xcassets"),
-          ]
+          dependencies: []
         ),
         .testTarget(
             name: "ExampleLibraryTests",
--- Sources/ExampleLibrary/Extensions/Colors.swift
+++ Sources/ExampleLibrary/Extensions/Colors.swift
@@ -10,65 +10,65 @@ import SwiftUI
 extension Color {
   // MARK: Complementary group
   /// Complementary Pastel Magenta hex color: #F9E4ED
-  static let pastelMagenta = Color("pastel_magenta")
+  static let pastelMagenta = Color("pastel_magenta", bundle: Bundle.module)
   /// Complementary Pastel Mint hex color: #E6F4F1
-  static let pastelMint = Color("pastel_mint")
+  static let pastelMint = Color("pastel_mint", bundle: Bundle.module)
   /// Complementary Pastel Terra hex color: #FEF6F0
-  static let pastelTerra = Color("pastel_terra")
+  static let pastelTerra = Color("pastel_terra", bundle: Bundle.module)
   /// Complementary Pastel cyan hex color: #E6F5FC
-  static let pastelCyan = Color("pastel_cyan")
+  static let pastelCyan = Color("pastel_cyan", bundle: Bundle.module)
 
   // MARK: Feedback group
   /// Feedback green paris hex color: #009372
-  static let greenParis = Color("green_paris")
+  static let greenParis = Color("green_paris", bundle: Bundle.module)
   /// Feedback red paris hex color: #EE595A
-  static let redParis = Color("red_paris")
+  static let redParis = Color("red_paris", bundle: Bundle.module)
   /// Feedback red paris hex color: #E96716
-  static let orangeParis = Color("orange_paris")
+  static let orangeParis = Color("orange_paris", bundle: Bundle.module)
 
   // MARK: Grayscale group
   /// Grayscale Background paris hex color: #F9F9F9
-  static let backgroundParis = Color("background_paris")
+  static let backgroundParis = Color("background_paris", bundle: Bundle.module)
   /// Grayscale Black paris hex color: #1A1A1A
-  static let blackParis = Color("black_paris")
+  static let blackParis = Color("black_paris", bundle: Bundle.module)
   /// Grayscale Light gray border hex color: #D2D2D2
-  static let lightGrayBorder = Color("light_gray_border")
+  static let lightGrayBorder = Color("light_gray_border", bundle: Bundle.module)
   /// Grayscale Silver hex color: #4D4D4D
-  static let silver = Color("silver")
+  static let silver = Color("silver", bundle: Bundle.module)
   /// Grayscale Skeleton hex color: #E2E2E2
-  static let skeleton = Color("skeleton")
+  static let skeleton = Color("skeleton", bundle: Bundle.module)
   /// Grayscale Pearl gray hex color: #F2F2F2
-  static let pearlGray = Color("pearl_gray")
+  static let pearlGray = Color("pearl_gray", bundle: Bundle.module)
   /// Grayscale Dark gray hex color: #818180
-  static let darkGray = Color("dark_gray")
+  static let darkGray = Color("dark_gray", bundle: Bundle.module)
   /// Grayscale High white hex color: Any: #FFFFFF - Dark: #1A1A1A
-  static let whiteParis = Color("high_white")
+  static let whiteParis = Color("high_white", bundle: Bundle.module)
   /// Grayscale High white hex color: Any: #FFFFFF - Dark: #000000
-  static let whiteTwoParis = Color("Background1")
+  static let whiteTwoParis = Color("Background1", bundle: Bundle.module)
 
   // MARK: Primary group
   /// Primary Cyan hex color: #006DFF
-  static let cyan = Color("cyan_paris")
+  static let cyan = Color("cyan_paris", bundle: Bundle.module)
   /// Primary Cyan strong hex color: #0057CB
-  static let cyanStrong = Color("cyan_strong")
+  static let cyanStrong = Color("cyan_strong", bundle: Bundle.module)
 
   // MARK: Secondary group
   /// Secondary Indigo hex color: #00355B
-  static let indigo = Color("indigo")
+  static let indigo = Color("indigo", bundle: Bundle.module)
   /// Secondary Ratings fill hex color: #FFC833
-  static let ratingsFill = Color("ratings_fill")
+  static let ratingsFill = Color("ratings_fill", bundle: Bundle.module)
 
   // MARK: Gammification group
   /// Gammification level 1 hex color: #00856A
-  static let gammificationLevel1 = Color("gammification_level1")
+  static let gammificationLevel1 = Color("gammification_level1", bundle: Bundle.module)
   /// Gammification level 2  hex color: #228091
-  static let gammificationLevel2 = Color("gammification_level2")
+  static let gammificationLevel2 = Color("gammification_level2", bundle: Bundle.module)
   /// Gammification level 3 hex color: #5E5EF7
-  static let gammificationLevel3 = Color("gammification_level3")
+  static let gammificationLevel3 = Color("gammification_level3", bundle: Bundle.module)
   /// Gammification level 4  hex color: #541CCC
-  static let gammificationLevel4 = Color("gammification_level4")
+  static let gammificationLevel4 = Color("gammification_level4", bundle: Bundle.module)
   /// Gammification level 5 hex color: #3E4F5B
-  static let gammificationLevel5 = Color("gammification_level5")
+  static let gammificationLevel5 = Color("gammification_level5", bundle: Bundle.module)
   /// Gammification level 6  hex color: #00355B
-  static let gammificationLevel6 = Color("gammification_level6")
+  static let gammificationLevel6 = Color("gammification_level6", bundle: Bundle.module)
 }
